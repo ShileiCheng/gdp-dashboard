d52e018b007cfd4073e49cd16858e0808d9b50ac
zhuoran627 <zw2121@nyu.edu>
2024-11-20 20:43:45 -0500

    added non pca

--- data_integration.py
+++ data_integration.py
@@ -4,27 +4,33 @@ from pathlib import Path
 # Define file paths
 ANOMALY_FILENAME = Path(__file__).parent / 'data' / 'overall_anomalies.csv'
 SVM_ANOMALIES_FILENAME = Path(__file__).parent / 'data' / 'svm_anomalies.csv'
-DBSCAN_ANOMALIES_FILENAME = Path(__file__).parent / 'data' / 'anomalies_dbscan_pca.csv'
+DBSCAN_PCA_FILENAME = Path(__file__).parent / 'data' / 'anomalies_dbscan_pca.csv'
 ISOLATION_ANOMALIES_FILENAME = Path(__file__).parent / 'data' / 'all_stock_anomalies_5th_threshold.csv'
+DBSCAN_NONPCA_FILENAME = Path(__file__).parent / 'data' / 'DBSCAN-nonpca_labels.csv'
 
 # Load data
 overall_anomalies = pd.read_csv(ANOMALY_FILENAME)
-
 overall_anomalies.rename(columns={'Overall_Anomaly': 'baseline'}, inplace=True)
 
 # Load additional datasets
 svm_anomalies = pd.read_csv(SVM_ANOMALIES_FILENAME)
-dbscan_anomalies = pd.read_csv(DBSCAN_ANOMALIES_FILENAME)
+dbscan_anomalies = pd.read_csv(DBSCAN_PCA_FILENAME)
 isolation_anomalies = pd.read_csv(ISOLATION_ANOMALIES_FILENAME)
+dbscan_nonpca_anomalies = pd.read_csv(DBSCAN_NONPCA_FILENAME)
 
 # Add 'svm_match' column: 1 if match, 0 otherwise
 overall_anomalies['svm'] = overall_anomalies.merge(
     svm_anomalies[['Ticker', 'Date']], on=['Ticker', 'Date'], how='left', indicator=True
 )['_merge'].eq('both').astype(int)
 
-# Add 'dbscan_match' column: 1 if match, 0 otherwise
-overall_anomalies['dbscan'] = overall_anomalies.merge(
+# Add 'dbscan_match' column (PCA): 1 if match, 0 otherwise
+overall_anomalies['dbscan_pca'] = overall_anomalies.merge(
     dbscan_anomalies[['Ticker', 'Date']], on=['Ticker', 'Date'], how='left', indicator=True
+)['_merge'].eq('both').astype(int)
+
+# Add 'dbscan_nonpca_match' column: 1 if match, 0 otherwise
+overall_anomalies['dbscan_nonpca'] = overall_anomalies.merge(
+    dbscan_nonpca_anomalies[['Ticker', 'Date']], on=['Ticker', 'Date'], how='left', indicator=True
 )['_merge'].eq('both').astype(int)
 
 # Add 'isolation_match' column: 1 if match, 0 otherwise
